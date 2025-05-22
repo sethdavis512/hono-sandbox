1591950be83113751b844c4800514b788bad85f8
Seth Davis <sethdavis512@gmail.com>
2025-05-21 21:15:57 -0500

    Update index

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,10 @@
+import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
-import type { FC } from 'hono/jsx';
 
 const app = new Hono();
 
-const Layout: FC = (props) => {
-    return (
-        <html>
-            <body>{props.children}</body>
-        </html>
-    );
-};
+app.get('/', (c) => c.text('Hello Node.js!'));
 
-const Top: FC<{ messages: string[] }> = (props: { messages: string[] }) => {
-    return (
-        <Layout>
-            <h1>Hello Hono!</h1>
-            <ul>
-                {props.messages.map((message) => {
-                    return <li>{message}!!</li>;
-                })}
-            </ul>
-        </Layout>
-    );
-};
+app.get('/:myParam', (c) => c.text(c.req.param('myParam')));
 
-app.get('/', (c) => {
-    const messages = ['Good Morning', 'Good Evening', 'Good Night'];
-    return c.html(<Top messages={messages} />);
-});
-
-export default app;
+serve(app);
